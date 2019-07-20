df19b619ce8ab9908e552d6c9c7e49a38b013b7b
Jefferson Corrêa Braga <jefferson.braga.correa@gmail.com>
2019-07-20 20:52:30 -0300

    Primeiro Commit

---
+++ dist/js/main.js
@@ -0,0 +1,8 @@
+function initMap(){
+   let options = {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+   }
+   let map = new google.maps.Map(document.getElementById('map'), options);
+}
+    
